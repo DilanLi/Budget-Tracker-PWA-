3bef7464dcfc8a314d2e583a5321c12598e82d34
Dilan Li <dilanli0706@gmail.com>
2020-09-29 13:41:33 -0600

    fixed typo in service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,10 +3,10 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/index.html",
   "/dist/manifest.json",
-  "/style.css",
+  "/styles.css",
   "/index.js",
   "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
+  "/icons/icon-512x512.png"
 ];
 
 // install
