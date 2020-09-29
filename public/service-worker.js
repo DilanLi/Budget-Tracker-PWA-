b752251bbdfb9dba03a8ad766120efc5e1d5cce6
Dilan Li <dilanli0706@gmail.com>
2020-09-29 15:12:15 -0600

    error handling in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -54,6 +54,26 @@ self.addEventListener('install', (event) => {
             });
           });
         })
-      );
+      )
+      self.addEventListener('fetch', (event) => {
+           if (event.request.url.startsWith(self.location.origin)) {
+             event.respondWith(
+               caches.match(event.request).then((cachedResponse) => {
+                 if (cachedResponse) {
+                   return cachedResponse;
+                 }
+         
+                 return caches.open(DATA_CACHE_NAME).then((cache) => {
+                   return fetch(event.request).then((response) => {
+                     return cache.put(event.request, response.clone()).then(() => {
+                       return response;
+                     });
+                   });
+                 });
+               })
+             )
+             .catch(err => console.log(err));
+           }
+         });
     }
   });
